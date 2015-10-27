05f4ab53d7131d8e3c9cbf9f670809a96d12533a
xd520 <327031193@qq.com>
2015-10-27 09:55:37 +0800

    Initial Commit

---
+++ EquityTrading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EquityTrading
+//
+//  Created by mac on 15/10/27.
+//  Copyright © 2015年 ApexSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
